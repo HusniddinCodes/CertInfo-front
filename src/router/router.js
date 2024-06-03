0bdf7927e08fb787b3c91f416ddfd98b3bbae749
umid.ravshanov91 <umid.ravshanov91@mail.ru>
2024-06-04 01:29:17 +0700

    Bind (CourseCreatePage to course.js)

--- src/router/router.js
+++ src/router/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path:'/createCertificate',
-    component:import('@/pages/createCertificatePage.vue')
+    component:import('@/pages/CreateCertificatePage.vue')
   },
   {
     path:'/create-course',
