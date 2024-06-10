5e4afdfd9d8307a44c5411258ea4c3a44b7cb1a4
xamroyevdostonbek07@gmail.com <xamroyevdostonbek07@gmail.com>
2024-06-10 16:46:28 +0000

    Update SignInComponent.vue

--- src/components/SignInComponent.vue
+++ src/components/SignInComponent.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-    name: "SignIn",
+    name: "SignInComponent",
     methods: {
         togglePasswordVisibility() {
             const password = document.getElementById("password");
