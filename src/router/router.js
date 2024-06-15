c43a7ffe56e9276efdb0cafe869281f7aa3b72a7
ibrohimbek <ibrohimbek4562@gmail.com>
2024-06-15 16:16:43 +0500

    add security to router for admin

--- src/router/router.js
+++ src/router/router.js
@@ -1,22 +1,49 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import store from "@/vuex/store.js"
 
 const routes = [
-  {
-    path:'/createCertificate',
-    component:import('@/pages/CreateCertificatePage.vue')
-  },
-  {
-    path:'/create-course',
-    component: () => import('@/pages/CourseCreatePage.vue')
-  },
-  {
-        path: '/certificate-info',
-        name: 'certificate-info',
-        component: import('@/pages/CertInfoPage.vue')
-  }
+    {
+        path: '/admin/create-certificate',
+        component: () => import('@/pages/CreateCertificatePage.vue')
+    },
+    {
+        path: '/admin/certificate-edit/:id',
+        component: () => import('@/pages/CreateCertificatePage.vue')
+    },
+    {
+        path: '/admin/create-course',
+        component: () => import('@/pages/CourseCreatePage.vue')
+    },
+    {
+        path: '/admin/certificates',
+        component: () => import('@/pages/CertificatesPage.vue')
+    },
+    {
+        path: '/admin/certificate-info/:certificateId',
+        component: () => import('@/pages/CertInfoPage.vue')
+    },
+    {
+        path: '/scan-qr/:certificate',
+        component: () => import('@/pages/CertInfoPage.vue')
+    },
+    {
+        path: '/sign-in',
+        component: () => import('@/pages/LoginPage.vue')
+    },
+
 ]
 
-export default createRouter({
+const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.path.startsWith('/admin') && !store.getters.isAdmin) {
+        next({path: '/sign-in'})
+    } else {
+        next()
+    }
+})
+
+export default router
